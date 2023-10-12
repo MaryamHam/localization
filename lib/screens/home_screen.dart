4ecce0e6ece782ab10a8251b0c35ab5436bebc11
MaryamHam <107949861+MaryamHam@users.noreply.github.com>
2023-10-13 02:08:17 +0400

    change in home page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,51 +8,115 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromARGB(255, 210, 207, 197),
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 130, 102, 13),
-        title: Text("${AppLocalizations.of(context)!.welcome} ${AppLocalizations.of(context)!.name} "),
-      ),
-      body: Center(
-        child: BlocBuilder<MainCubit, MainState>(
-        builder: (context, state) {
-
-          return Column(
-            children: [
-              SizedBox(height: 25,),
-            Text(AppLocalizations.of(context)!.screen,style: TextStyle(color:Color.fromARGB(255, 130, 102, 13), 
-                fontSize: 25,fontWeight: FontWeight.bold, ),),
-
-                Container(
-                  padding: EdgeInsets.all(30),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: AppLocalizations.of(context)!.hint,
-                      hintStyle: TextStyle(
-                        color:Color.fromARGB(255, 130, 102, 13), 
-                      
-                      )
-                    ),
+    return SafeArea(
+      child: Scaffold(
+        // backgroundColor: Color.fromARGB(255, 210, 207, 197),
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 19, 43, 99),
+          title: Text("${AppLocalizations.of(context)!.welcome} ",style: TextStyle(color: Color.fromARGB(255, 233, 93, 17)),),
+        ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: BlocBuilder<MainCubit, MainState>(
+            builder: (context, state) {
+            
+              return Column(
+                children: [
+                   SizedBox(height: 25,),
+                  CircleAvatar(
+                    backgroundImage: AssetImage("assets/log.jpg"),
+                    radius: 80,
                   ),
-                ),
+                  SizedBox(height: 25,),
+                Text(AppLocalizations.of(context)!.screen,style: TextStyle(color:Color.fromARGB(255, 233, 93, 17), 
+                    fontSize: 25,fontWeight: FontWeight.bold, ),),
+             SizedBox(height: 25,),
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal:30,vertical: 3),
+                      child: TextField(
+                        decoration: InputDecoration(
+                           filled: true,
+                          fillColor: Color.fromARGB(255, 209, 230, 245),
+                          hintText: AppLocalizations.of(context)!.hint,
+                          hintStyle: TextStyle(
+                            color:Color.fromARGB(255, 233, 93, 17)),
+                               focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              borderSide: BorderSide(
+                                color: Color.fromARGB(255, 19, 43, 99),
+                              ),
+                             ),
+                        enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              borderSide: BorderSide(
+                                color: Color.fromARGB(255, 19, 43, 99),
+                              ),
+                             ),
+                        ),
+                      ),
+            
+                    ),
+            
+                    Container(
+                      padding: EdgeInsets.all(30),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Color.fromARGB(255, 209, 230, 245),
+                          hintText: AppLocalizations.of(context)!.pass,
+                          hintStyle: TextStyle(
+                            color:Color.fromARGB(255, 233, 93, 17)),
+                               focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide(
+                        color: Color.fromARGB(255, 19, 43, 99),
+                      ),
+                    ),
+                     enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              borderSide: BorderSide(
+                                color: Color.fromARGB(255, 19, 43, 99),
+                              ),
+                             ),
+                        ),
+                      ),
+                      
+                    ),
+            
+                    Container(
+                       width: MediaQuery.sizeOf(context).width *.65 ,
+                      height: MediaQuery.sizeOf(context).height *.05,
+                      child: MaterialButton(
+                        color:  Color.fromARGB(255, 33, 64, 134),
+                        
+                              onPressed: (){},
+                              child: Text(AppLocalizations.of(context)!.button,
+                             style: TextStyle(color: Color.fromARGB(255, 233, 93, 17)),
+                             ),
+                        
+                             ),
+                    ),
+             SizedBox(height: 50,),
+                  SwitchListTile(
+                    value: MainCubit.get(context).currentLang == ("en", " "),
+                    onChanged: (value) {
+                      if (AppLocalizations.of(context)!.localeName == "ar") {
+                        MainCubit.get(context).changeToEn();
+                      } else {
+                        MainCubit.get(context).changeToAr();
+                      }
+                    },
+                    title: Text(AppLocalizations.of(context)!.changelanguage,style: TextStyle(color:Color.fromARGB(255, 33, 64, 134), 
+                    fontSize: 16,fontWeight: FontWeight.bold ),),
+                  ),
 
-              SwitchListTile(
-                value: MainCubit.get(context).currentLang == ("en", " "),
-                onChanged: (value) {
-                  if (AppLocalizations.of(context)!.localeName == "ar") {
-                    MainCubit.get(context).changeToEn();
-                  } else {
-                    MainCubit.get(context).changeToAr();
-                  }
-                },
-                title: Text(AppLocalizations.of(context)!.changelanguage,style: TextStyle(color:Color.fromARGB(255, 130, 102, 13), 
-                fontSize: 16,fontWeight: FontWeight.bold ),),
-              ),
-            ],
-          );
-        },
-      )),
+
+                ],
+              );
+            },
+          )),
+        ),
+      ),
     );
   }
 }
